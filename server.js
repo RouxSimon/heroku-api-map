95b8e09116a4d6bf18999bb21252ed747f196395
RouxSimon <sim.roux1@gmail.com>
2020-11-18 00:34:31 +0100

    Demo

--- server.js
+++ server.js
@@ -14,11 +14,30 @@ app.get('/', function (req, res) {
   res.render('index', {weather: null, error: null});
 })
 
+
+
+var tomtom_geocode_api = 'http://api.tomtom.com/lbs/services/geocode/4/geocode?key=%s&query=Pizza&L=Pune&format=json';
+let tomtom_geocode_api2 = 'http://api.tomtom.com/map/1/staticimage?key=${apiKeyTomTom}&zoom=6&center=4.563,44.11&format=jpg&layer=basic&style=main&width=1305&height=748&view=Unified&language=fr-FR';
+
+router.get('/map', function(req, res) {
+
+	http.get(util.format(tomtom_geocode_api, req.key), function(response) {
+		var result = "";
+		response.on('data', function(chunk) {
+			result += chunk;
+		});
+		
+		response.on('end', function() {
+			res.json(result);
+		});
+	}).end();
+	
+});
+
 app.post('/', function (req, res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-  let urlTomTom = `http://api.tomtom.com/map/1/staticimage?key=${apiKeyTomTom}&zoom=6&center=4.563,44.11&format=jpg&layer=basic&style=main&width=1305&height=748&view=Unified&language=fr-FR`
-  
+
   console.log("URL: "+url)
   console.log("URL: "+urlTomTom)
   request(url, function (err, response, body) {
@@ -37,26 +56,6 @@ app.post('/', function (req, res) {
         res.render('index', {weather: weatherText, error: null});
       }
     }
-  });
-
-  request(urlTomTom, function (err, response, body) {
-    console.log("Status "+response.statusCode);
-
-    if(response.statusCode == 401){
-      res.render('index', {staticimage: null, error: 'Invalid API token'});
-    } else if(response.statusCode == 404){
-      res.render('index', {staticimage: null, error: 'Map not found'});
-    } else {
-      let staticimage = JSON.parse(body)
-
-      if(staticimage.main == undefined){
-        res.render('index', {staticimage: null, error: 'Error, please try again'});
-      } else {
-        let staticimageDisplay = `${staticimage}`;
-        res.render('index', {staticimage: staticimageDisplay, error: null});
-      }
-    }
-
   });
 })
 
