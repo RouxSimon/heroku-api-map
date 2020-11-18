44e6fb03520f96c2493434d6d966cdb71ed9ab06
RouxSimon <sim.roux1@gmail.com>
2020-11-18 19:29:55 +0100

    Demo

--- server.js
+++ server.js
@@ -15,23 +15,8 @@ app.get('/', function (req, res) {
 })
 
 
-
-var tomtom_geocode_api2 = 'http://api.tomtom.com/lbs/services/geocode/4/geocode?key=%s&query=Pizza&L=Pune&format=json';
-let tomtom_geocode_api = `http://api.tomtom.com/map/1/staticimage?key=${apiKeyTomTom}&zoom=6&center=4.563,44.11&format=jpg&layer=basic&style=main&width=1305&height=748&view=Unified&language=fr-FR`
-
-app.get('/map', function(req, res) {
-
-	http.get(util.format(tomtom_geocode_api, req.key), function(response) {
-		var result = "";
-		response.on('data', function(chunk) {
-			result += chunk;
-		});
-		
-		response.on('end', function() {
-			res.json(result);
-		});
-	}).end();
-	
+app.get('/map',  function () { 
+	window.open(`http://api.tomtom.com/map/1/staticimage?key=${apiKeyTomTom}&zoom=6&center=4.563,44.11&format=jpg&layer=basic&style=main&width=1305&height=748&view=Unified&language=fr-FR`);	
 });
 
 app.post('/', function (req, res) {
