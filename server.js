c1e7b9dd1fe649512fc8f4e6fd78c948d389a98f
RouxSimon <sim.roux1@gmail.com>
2020-11-18 21:27:49 +0100

    map display

--- server.js
+++ server.js
@@ -14,36 +14,28 @@ app.get('/', function (req, res) {
   res.render('index', {weather: null, error: null});
 })
 
-
-
-
-app.get('/map',  function (_req, res) { 
-  res.json({url: `https://api.tomtom.com/map/1/staticimage?layer=basic&style=main&format=png&center=4.65%2C%2045.11&width=512&height=512&view=Unified&key=${apiKeyTomTom}`});
-});
-
-app.get('/map2',  function (_req, res) { 
-  res.json({url: `http://api.tomtom.com/map/1/staticimage?key=${apiKeyTomTom}&zoom=6&center=4.563,44.11&format=jpg&layer=basic&style=main&width=1305&height=748&view=Unified&language=fr-FR`});
-});
-
 app.post('/', function (req, res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-
+  let url2 = `https://api.tomtom.com/map/1/staticimage?layer=basic&style=main&format=png&center=4.65%2C%2045.11&width=512&height=512&view=Unified&key=${apiKeyTomTom}`
+  let url3 = `http://api.tomtom.com/map/1/staticimage?key=${apiKeyTomTom}&zoom=6&center=4.563,44.11&format=jpg&layer=basic&style=main&width=1305&height=748&view=Unified&language=fr-FR`
+  
+  
   console.log("URL: "+url)
   request(url, function (err, response, body) {
     console.log("Status "+response.statusCode);
 
     if(response.statusCode == 401){
-      res.render('index', {weather: null, error: 'Invalid API token'});
-    }else if(response.statusCode == 404){
-      res.render('index', {weather: null, error: 'City not found'});
+      res.render('index', {url2: null, url3: null, weather: null, error: 'Invalid API token'});
+    } else if(response.statusCode == 404){
+      res.render('index', {url2: null, url3: null, weather: null, error: 'City not found'});
     } else {
       let weather = JSON.parse(body)
       if(weather.main == undefined){
-        res.render('index', {weather: null, error: 'Error, please try again'});
+        res.render('index', {url2: null, url3: null, weather: null, error: 'Error, please try again'});
       } else {
         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
+        res.render('index', {url2: url2, url3: url3, weather: weatherText, error: null});
       }
     }
   });
