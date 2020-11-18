e3dcd022f7bbadebd3d82f68c7c039c4015619d7
RouxSimon <sim.roux1@gmail.com>
2020-11-18 20:58:36 +0100

    map return url

--- server.js
+++ server.js
@@ -14,12 +14,15 @@ app.get('/', function (req, res) {
   res.render('index', {weather: null, error: null});
 })
 
-`https://api.tomtom.com/map/1/staticimage?layer=basic&style=main&format=png&center=4.65%2C%2045.11&width=512&height=512&view=Unified&key=${apiKeyTomTom}`
-`http://api.tomtom.com/map/1/staticimage?key=${apiKeyTomTom}&zoom=6&center=4.563,44.11&format=jpg&layer=basic&style=main&width=1305&height=748&view=Unified&language=fr-FR`
+
+
 
 app.get('/map',  function () { 
+  res.json({url: `https://api.tomtom.com/map/1/staticimage?layer=basic&style=main&format=png&center=4.65%2C%2045.11&width=512&height=512&view=Unified&key=${apiKeyTomTom}`});
+});
 
- 
+app.get('/map2',  function () { 
+  res.json({url: `http://api.tomtom.com/map/1/staticimage?key=${apiKeyTomTom}&zoom=6&center=4.563,44.11&format=jpg&layer=basic&style=main&width=1305&height=748&view=Unified&language=fr-FR`});
 });
 
 app.post('/', function (req, res) {
